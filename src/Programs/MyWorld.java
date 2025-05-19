f0c0c5e5ebaab8476d1f196b6e2b985fe04f6e10
Nithi <nithi5293@gmail.com>
2025-05-19 19:15:51 +0530

    initial commit

---
+++ src/Programs/MyWorld.java
@@ -0,0 +1,9 @@
+package Programs;
+
+public class MyWorld {
+    public static void main(String[] args) {
+        System.out.println("Subramanian");
+        System.out.println("Nithi devi");
+        System.out.println("Vainavi");
+    }
+}
