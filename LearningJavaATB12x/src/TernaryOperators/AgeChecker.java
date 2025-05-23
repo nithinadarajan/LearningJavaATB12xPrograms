69116c2f04c598eacc67bc33dcdf0476015f26d2
Nithi <nithi5293@gmail.com>
2025-05-23 19:19:37 +0530

    Fourth commit

---
+++ LearningJavaATB12x/src/TernaryOperators/AgeChecker.java
@@ -0,0 +1,19 @@
+package TernaryOperators;
+
+import java.util.Scanner;
+
+public class AgeChecker {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		int age = sc.nextInt();
+		String category = (age <= 18)  ? "Minor" : (age > 65) ? "Senior"  : "Adult";
+
+                   
+		System.out.println("You are a(n): " + category);
+
+		sc.close();
+	}
+
+}
---
+++ LearningJavaATB12x/src/TernaryOperators/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package TernaryOperators;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		Scanner  sc=new Scanner(System.in);
+		System.out.println("Enter the No.");
+		int No=sc.nextInt(); //input->No entered by user
+		
+		String result = (No%2==0) ? "even" : "odd"; //output->string->even or odd
+		System.out.println(result);
+
+	}
+
+}
