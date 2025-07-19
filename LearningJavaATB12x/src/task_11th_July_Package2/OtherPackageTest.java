c58a6cada63dea6c7ddefbc5e724262254b949a2
Nithi <nithi5293@gmail.com>
2025-07-19 21:21:34 +0530

    commit

---
+++ LearningJavaATB12x/src/task_11th_July_Package2/OtherPackageTest.java
@@ -0,0 +1,13 @@
+package task_11th_July_Package2;
+
+import task_11th_July_Package1.Student;
+
+public class OtherPackageTest {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+        student.showInfo();  // Works fine, different package too!
+
+	}
+
+}
